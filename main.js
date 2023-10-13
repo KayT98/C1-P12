95dc7a5f2844e270e2cfd28bd27135666bca5e82
KayT98 <khanhtruong109@yahoo.com>
2023-10-12 23:30:16 -0500

    add datas for these files

---
+++ main.js
@@ -0,0 +1,38 @@
+const Answers = [
+    {
+        question: "What is the capital of the US?",
+        answers: [
+            "Washington D.C",
+            "New York",
+            "Los Angeles",
+            "Dallas"
+        ],
+        correctAnswer: 0
+    },
+
+    {
+        question: "What is 100 + 200?",
+        answers: [
+            "4545",
+            "22323",
+            "300",
+            "7777"
+        ],
+        correctAnswer: 2
+    },
+
+    {
+        question: "Who is the founder of Amazon?",
+        answers: [
+            "Steve Jobs",
+            "Bill Gates",
+            "You",
+            "Jeff Bezos"
+        ],
+        correctAnswer: 3
+    }
+]
+
+let currentQuestion = 0;
+let score = 0;
+
