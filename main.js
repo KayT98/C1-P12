706b4800e67915cd88de0a52d21d2bb574c2db9c
KayT98 <khanhtruong109@yahoo.com>
2023-10-14 12:49:46 -0500

    jquery added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Answers = [
+const quizApp = [
     {
         question: "What is the capital of the US?",
         answers: [
@@ -36,3 +36,58 @@ const Answers = [
 let currentQuestion = 0;
 let score = 0;
 
+const questions = $('#questions');
+const choices = $('#answers');
+const result = $('#result');
+const nextBtn = $('#next');
+const prevBtn = $('#prev');
+
+function displayQuestion() {
+    questions.text(quizApp[currentQuestion].question);
+    choices.empty(); //hide the answers
+
+    quizApp[currentQuestion].answers.forEach((answer, i) => {
+        const answerChoices = $(`<li class="answer">${answer}</li>`);
+        answerChoices.click(() => answerCheck(i));
+        choices.append(answerChoices)
+    })
+}
+
+
+function answerCheck(userChoice) {
+    const correct = quizApp[currentQuestion].correctAnswer;
+    
+    if(userChoice == correct) {
+        score++;
+        result.text("You got it right!").css('color', 'green');
+    }
+    else {
+        result.text("Correct answer is: " + quizApp[currentQuestion].answers[correctAnswer]).css('color', 'red');
+    }
+    nextBtn.show();
+    $('.answer').off('click') //disable other answers after the user chose the answer
+}
+
+$("#next").click(function(){
+    if (currentQuestion < quizApp.length - 1) {
+        currentQuestion++;
+        displayQuestion();
+        nextBtn.hide();
+        result.empty();
+    }
+    else {
+        showResult();
+    }
+});
+
+function showResult() {
+    questions.text(`You scored ${score} out of ${quizApp.length}`)
+    choices.empty();
+    result.empty();
+    nextBtn.hide();
+    prevBtn.hide();
+}
+ 
+
+
+displayQuestion();
